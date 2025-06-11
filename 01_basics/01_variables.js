bf16ea2d36c49b89497ff90eb90ae5fa26c858c3
HarshVarma24 <harshvarma242@gmail.com>
2025-06-11 20:45:50 +0530

    Code has been added

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountEmail = "harsh@gmail.com"
+var accountPassword = "12345"
+accountCity = "Delhi" 
+let accountState
+// not recommended to use var for new code
+// const is used for constants, which cannot be reassigned
+// accountId = 2 // not allowed, const cannot be reassigned
+
+accountEmail = "hardy@gmail.com"
+accountPassword = "478589"
+accountCity = "Mumbai"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,14 @@
+let name = "harsh"
+let age = 21
+let isLoggedIn = true
+// number => 2 to power of 53
+// bigint => 2 to power of 64
+// string => "harsh", 'harsh', `harsh`
+// boolean => true, false
+// null => empty value
+// undefined => variable is declared but not initialized
+// symbol => unique value, used for unique identifiers
+// object => key-value pairs
+console/log(typeof "harsh") // string
+console.log(typeof null) // object (this is a bug in JavaScript)
+console.log(typeof undefined) // undefined
