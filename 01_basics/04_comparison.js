900c61403ccbdbda66ff8b1e4015d6b12a2a5a68
HarshVarma24 <harshvarma242@gmail.com>
2025-06-13 02:35:13 +0530

    code added successfuly

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,23 @@
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+console.log("2" < "1");
+console.log("02" > 1 );
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+// not a good practice to compare null with numbers
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// not a good practice to compare undefined with numbers
+
+
+// // === 
+// === is strict equality operator, // it checks both value and type
+
+console.log("2" === 1);
---
+++ 01_basics/datatype summary.js
@@ -0,0 +1,40 @@
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// symbols are unique and immutable, even if they have the same description
+// symobols are used to create unique identifiers.
+// what are symbols?
+// they are a new primitive data type in JavaScript, introduced in ES6.
+
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+myFunction();
+
+console.log(typeof heros);
