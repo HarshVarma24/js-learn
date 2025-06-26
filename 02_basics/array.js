cd2ff52078cd8d5bc8c7790dbb02ca8e677c5811
HarshVarma24 <harshvarma242@gmail.com>
2025-06-26 20:01:53 +0530

    array updated

--- 02_basics/array.js
+++ 02_basics/array.js
@@ -50,5 +50,34 @@ const myn2= myarrray.splice(1,3);
 console.log("C", myarrray) // original array is modified
 console.log(myn2)
  // original array is modified
+ 
+ const marvel = ["thor", "doctor strange", "ironman", "spiderman"];
+ const dc = ["superman", "batman", "flash", "aquaman"];
+ 
+ marvel.push(dc)
+ console.log(marvel); 
+ console.log(marvel.length); // 5
+ console.log(marvel[3]); // "spiderman"
+ 
+ const allheros =  marvel.concat(dc);
+ console.log(allheros); // ["thor", "doctor strange", "ironman", "spiderman", "superman", "batman", "flash", "aquaman"])
+ 
+const all_new_heros = [...marvel, ...dc];//spead operator
+console.log(all_new_heros);
+
+const tipicalArray = [1, 2, 3, [4, 5, 6], 7, 8, 9];
+const flatArray = tipicalArray.flat(); // flattens the array
+console.log(flatArray); // [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+console.log(Array.isArray("Harsh"))// false
+console.log(Array.from("Harsh")) // ["H", "a", "r", "s", "h"]
+console.log(Array.from({name :"Aarav"}))// // [undefined]
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+console.log(Array.of(score1, score2, score3)) // [100, 200, 300]
 
 
+
+ 
