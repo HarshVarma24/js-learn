691752ab120a7471b37bba66892196c5c410694c
HarshVarma24 <harshvarma242@gmail.com>
2025-07-08 19:39:21 +0530

    basics over, new topics started

---
+++ 04_basics/cflow.js
@@ -0,0 +1,15 @@
+//CONTROL FLOW
+// 1. if-else statement
+
+//if(condition){
+    // code to execute if condition is true
+
+
+const balance = 1000;
+//if(balance > 500) console.log("You have enough balance.");
+
+const userLogin = true;
+const card = true
+if(userLogin && card) {
+    console.log("You can access your account.");
+}
